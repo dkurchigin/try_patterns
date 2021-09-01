fa777a39143d8014646cba08fdbe0b1afc3deae6
dkurchigin <kurchigin.dmitry@yandex.ru>
2021-09-01 13:24:30 +0300

    add factory

---
+++ creational patterns/factory_method.py
@@ -0,0 +1,62 @@
+""" EXAMPLE FOR FACTORY METHOD IN PYTHON """
+from abc import ABC, abstractmethod
+
+
+class AbstractTransport(ABC):
+    """ABSTRACT CLASS"""
+    def __init__(self):
+        self.speed = 0
+        self.price = 0
+
+    @abstractmethod
+    def show_max_speed(self):
+        """SHOW MAX SPEED"""
+        pass
+
+    @abstractmethod
+    def show_price(self):
+        """SHOW PRICE"""
+        pass
+
+
+class MotoTransport(AbstractTransport):
+    """CONCRETE MOTO CLASS"""
+    def __init__(self):
+        super().__init__()
+        self.speed = 100
+        self.price = 2000
+
+    def show_max_speed(self):
+        print(f'Max speed for moto = {self.speed}')
+
+    def show_price(self):
+        print(f'Price for moto = {self.price}')
+
+
+class AutoTransport(AbstractTransport):
+    """CONCRETE AUTO CLASS"""
+    def __init__(self):
+        super().__init__()
+        self.speed = 80
+        self.price = 4000
+
+    def show_max_speed(self):
+        print(f'Max speed for auto = {self.speed}')
+
+    def show_price(self):
+        print(f'Price for auto = {self.price}')
+
+
+def create_transport(transport_type):
+    """RETURN CREATED CLASS"""
+    return transport_type()
+
+
+if __name__ == '__main__':
+    various_transport = {'moto': MotoTransport, 'auto': AutoTransport}
+
+    for one_type, transport_class in various_transport.items():
+        print(f'\n***{one_type}***')
+        one_transport = create_transport(transport_class)
+        one_transport.show_max_speed()
+        one_transport.show_price()
